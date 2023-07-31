05f2e34cfab7f956e5c6315a932c18025a79dc0f
Evgenij Ostapenko <e.ostapenko@fast-report.com>
2023-07-31 14:47:38 +0300

    add load_more feature

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -4,8 +4,11 @@ class ArtistsController < ApplicationController
     albums = selected_albums(artist.albums, params[:album_type]).with_attached_cover.preload(:artist)
     tracks = artist.tracks.popularity_ordered.limit(5)
 
-    if turbo_frame_request?
+    if turbo_frame_request_id match? /discography/
       render partial: "discography", locals: {artist:, albums:}
+    elsif turbo_frame_request_id match? /popular/
+      tracks = artist.tracks.popularity_ordered.limit(10)
+      render partial: "popular", locals: {artist:, tracks:, expand_link: false}
     else
       render action: :show, locals: {artist:, albums:, tracks:}
     end
